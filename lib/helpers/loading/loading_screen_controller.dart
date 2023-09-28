2ac3a508ad2884e02b7a5e773d8480f39487daf4
kinsomaz <Kinsomaz87@gmail.com>
2023-09-28 12:48:23 +0100

    step-26

---
+++ lib/helpers/loading/loading_screen_controller.dart
@@ -0,0 +1,11 @@
+typedef CloseLoadingScreen = bool Function();
+typedef UpdateLoadingScreen = bool Function(String text);
+
+class LoadingScreenController {
+  final CloseLoadingScreen close;
+  final UpdateLoadingScreen update;
+  LoadingScreenController({
+    required this.close,
+    required this.update,
+  });
+}
